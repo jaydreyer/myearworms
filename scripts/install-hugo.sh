5c23688ad2ad1f9b0650b53454694241490b1b57
Jay Dreyer <jay.dreyer@target.com>
2017-02-24 20:28:20 -0600

    Updating deploy script

--- scripts/install-hugo.sh
+++ scripts/install-hugo.sh
@@ -2,4 +2,4 @@
 HUGO_VERSION=0.18.1
 wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar xvzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-cp hugo_${HUGO_VERSION}_Linux-64bit/hugo_${HUGO_VERSION}_Linux-64bit hugo
+cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 hugo
