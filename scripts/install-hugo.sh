d6a1f6295a7627113563a5a4495cd04c2c14d6c2
Jay Dreyer <jay.dreyer@target.com>
2017-03-01 21:10:55 -0600

    Update script

--- scripts/install-hugo.sh
+++ scripts/install-hugo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
 HUGO_VERSION=0.19
 HUGULP_VERSION=1.1.1
+mkdir -p ~/bin
+cd ~/bin 
 wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar xvzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 hugo
