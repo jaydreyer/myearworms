fa3f8c7da3ea0bf4dd75984e671abea1b7fb5fe0
Jay Dreyer <jay.dreyer@gmail.com>
2018-02-11 10:49:23 -0600

    more

--- scripts/install-hugo.sh
+++ scripts/install-hugo.sh
@@ -9,5 +9,5 @@ cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 hugo
 #cwget https://github.com/jbrodriguez/hugulp/archive/${HUGULP_VERSION}.tar.gz
 # tar xvzf ${HUGULP_VERSION}.tar.gz
 # cp hugulp-${HUGULP_VERSION} hugo
-npm install
+# npm install
 # npm install -g https://github.com/jaydreyer/hugulp/tarball/master
