383f5da22f538f83e2263656d2362f6209210913
Jay Dreyer <jay.dreyer@gmail.com>
2018-02-11 10:47:54 -0600

    deploy problems

--- scripts/install-hugo.sh
+++ scripts/install-hugo.sh
@@ -6,8 +6,8 @@ cd ~/bin
 wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar xvzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 hugo
-wget https://github.com/jbrodriguez/hugulp/archive/${HUGULP_VERSION}.tar.gz
-tar xvzf ${HUGULP_VERSION}.tar.gz
-cp hugulp-${HUGULP_VERSION} hugo
+#cwget https://github.com/jbrodriguez/hugulp/archive/${HUGULP_VERSION}.tar.gz
+# tar xvzf ${HUGULP_VERSION}.tar.gz
+# cp hugulp-${HUGULP_VERSION} hugo
 npm install
 # npm install -g https://github.com/jaydreyer/hugulp/tarball/master
